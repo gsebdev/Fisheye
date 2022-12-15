30f3053b4f1af2e06bdfdb4fdc4551218e45f4d2
Sebastien Gault <sgault.webdev@gmail.com>
2022-12-15 13:58:51 +0100

    création de la fonctionnalité trier par:

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -17,7 +17,7 @@ async function getPhotographer(id) {
         .catch(error => console.log('erreur de récupération des données ', error))
 }
 
-async function displayHeaderData(photographer) {
+function displayHeaderData(photographer) {
     const photographerModel = new PhotographerModel(photographer)
     const title = document.querySelector('h1')
     const location = document.querySelector('.photograph-header__location')
@@ -34,10 +34,44 @@ async function displayHeaderData(photographer) {
     likes.innerHTML = photographerModel.likes + ' <img src="assets/icons/heart.svg" alt="likes">'
 }
 
+async function handleFilterChange(medias, FilterInstance) {
+    FilterInstance.filterChange
+        .then(filter => {
+            displayMedias(medias, filter)
+            handleFilterChange(medias, FilterInstance)
+        })
+}
+
+function displayMedias(medias, filter) {
+    medias.sort((a, b) => {
+        if(a[filter] > b[filter]) {
+            return 1
+        }
+        if(a[filter] < b[filter]) {
+            return -1
+        }
+        return 0
+    })
+    if(filter !== 'title') {
+        medias.reverse()
+    }
+
+
+    const wrapper = document.querySelector('.photograph-medias')
+    wrapper.querySelectorAll('p').forEach(p => p.remove())
+    medias.forEach(media => {
+        const p = document.createElement('p')
+        p.textContent = media.date + ' /likes:' + media.likes + ' /title:' + media.title
+        wrapper.appendChild(p)
+    })
+}
+
 async function init() {
     const photographer = await getPhotographer(photographerID)
     displayHeaderData(photographer)
-    displayMedias(photographer.media)
+   const filters = new FilterSortBy('.sort-filter__menu', '.sort-filter__filter')
+   displayMedias(photographer.media, filters.filterValue)
+   handleFilterChange(photographer.media, filters)
 
 }
 
---
+++ scripts/utils/FilterSortBy.js
@@ -0,0 +1,60 @@
+class FilterSortBy {
+    /**
+     * 
+     * @param {String} menuSelector 
+     * @param {String} filterSelector
+     */
+    constructor(menuSelector, filterSelector) {
+        this._currentFilterElement = document.querySelector(filterSelector + '[data-selected=true]')
+        this.currentFilterValue = this._currentFilterElement.getAttribute('data-value')
+        this._isExpanded = false
+        this._menu = document.querySelector(menuSelector)
+        this._filters = document.querySelectorAll(filterSelector)
+        this._triggerFilterChange
+        this._filters.forEach(filter => {
+            filter.addEventListener('click', this.handleFilterClick.bind(this))
+        })
+    }
+    handleFilterClick(e) {
+        if(this._isExpanded) {
+            const clickedFilter = e.target
+            
+            clickedFilter.getAttribute('data-selected') != 'true' ? 
+                this.switchSelectedFilter(clickedFilter) : 
+                this.closeFilterMenu()
+                
+
+        } else {
+
+            this.openFilterMenu()
+
+        }
+    }
+    switchSelectedFilter(newFilterElement) {
+        this._currentFilterElement.setAttribute('data-selected', false)
+        newFilterElement.setAttribute('data-selected', true)
+        this._currentFilterElement = newFilterElement
+        this.currentFilterValue = newFilterElement.getAttribute('data-value')
+        this.closeFilterMenu()
+        this._triggerFilterChange(this.currentFilterValue)
+
+
+    }
+    closeFilterMenu() {
+        this._menu.classList.remove('dropped-down')
+        this._isExpanded = false
+    }
+    openFilterMenu() {
+        this._menu.classList.add('dropped-down')
+        this._isExpanded = true
+    }
+
+    get filterChange() {
+        return new Promise(resolve => {
+            this._triggerFilterChange = resolve
+        })
+    }
+    get filterValue() {
+        return this.currentFilterValue
+    }
+}
